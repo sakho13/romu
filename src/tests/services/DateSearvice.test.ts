884e12069b46f164d10dfc45c277e0b0159fedf4
sakho13 <wecl.khotaro@gmail.com>
2024-11-10 15:25:13 +0900

    ✅ DateServiceのユニットテストを追加

---
+++ src/tests/services/DateSearvice.test.ts
@@ -0,0 +1,78 @@
+import { DateService } from "@/services/DateService"
+
+describe("services/DateService", () => {
+  describe("dateToYYYYMMDD", () => {
+    test("Date型の日付をYYYYMMDD型に変換する", () => {
+      expect(DateService.dateToYYYYMMDD(new Date(2022, 1, 1))).toEqual({
+        year: 2022,
+        month: 2,
+        day: 1,
+      })
+      expect(DateService.dateToYYYYMMDD(new Date(2022, 1, 28))).toEqual({
+        year: 2022,
+        month: 2,
+        day: 28,
+      })
+      expect(DateService.dateToYYYYMMDD(new Date(2022, 0, 1))).toEqual({
+        year: 2022,
+        month: 1,
+        day: 1,
+      })
+    })
+  })
+
+  describe("yyyymmddToDate", () => {
+    test("YYYYMMDD型の日付をDate型に変換する", () => {
+      expect(
+        DateService.yyyymmddToDate({ year: 2022, month: 2, day: 1 }),
+      ).toEqual(new Date(2022, 1, 1))
+      expect(
+        DateService.yyyymmddToDate({ year: 2022, month: 2, day: 28 }),
+      ).toEqual(new Date(2022, 1, 28))
+      expect(
+        DateService.yyyymmddToDate({ year: 2022, month: 1, day: 1 }),
+      ).toEqual(new Date(2022, 0, 1))
+    })
+  })
+
+  describe("shiftMonth", () => {
+    test("月を指定した数だけ移動する", () => {
+      expect(DateService.shiftMonth({ year: 2022, month: 1 }, 1)).toEqual({
+        year: 2022,
+        month: 2,
+      })
+      expect(DateService.shiftMonth({ year: 2022, month: 1 }, 2)).toEqual({
+        year: 2022,
+        month: 3,
+      })
+      expect(DateService.shiftMonth({ year: 2022, month: 1 }, -1)).toEqual({
+        year: 2021,
+        month: 12,
+      })
+      expect(DateService.shiftMonth({ year: 2022, month: 1 }, -2)).toEqual({
+        year: 2021,
+        month: 11,
+      })
+    })
+  })
+
+  describe("getStartDate", () => {
+    test("月の初日週の日曜の日付を取得する", () => {
+      expect(
+        DateService.dateToYYYYMMDD(
+          DateService.getStartDate({ year: 2024, month: 11 }),
+        ),
+      ).toStrictEqual({ year: 2024, month: 10, day: 27 })
+    })
+  })
+
+  describe("getEndDate", () => {
+    test("月の最終日週の土曜の日付を取得する", () => {
+      expect(
+        DateService.dateToYYYYMMDD(
+          DateService.getEndDate({ year: 2024, month: 11 }),
+        ),
+      ).toStrictEqual({ year: 2024, month: 11, day: 30 })
+    })
+  })
+})
