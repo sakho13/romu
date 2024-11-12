cce09659cef71dae1627c9bc56fbb9d2e9006052
sakho13 <wecl.khotaro@gmail.com>
2024-11-13 07:44:19 +0900

    ✨ useCalenderフックを新規作成し、月の移動と日付生成機能を実装

---
+++ src/services/hooks/useCalender.ts
@@ -0,0 +1,47 @@
+import { YYYYMM, YYYYMMDD } from "@/types/DateTimeType"
+import { useState } from "react"
+import { DateService } from "../DateService"
+
+type StoredDays = {
+  [key: string]: number[][]
+}
+
+export function useCalender(initDate: Date = new Date()) {
+  const [mainMonth, setMainMonth] = useState<YYYYMM>(
+    DateService.dateToYYYYMMDD(initDate),
+  )
+  const [selectedDate, setSelectedDate] = useState<YYYYMMDD>(
+    DateService.dateToYYYYMMDD(initDate),
+  )
+  const [weeks, setWeeks] = useState<number[][]>([])
+
+  function prevMonth() {
+    setMainMonth(DateService.shiftMonth(mainMonth, -1))
+  }
+
+  function nextMonth() {
+    setMainMonth(DateService.shiftMonth(mainMonth, +1))
+  }
+
+  function initCalender(mainMonth: YYYYMM) {
+    // 前後の月分も生成する
+    // 月を移動してした場合、追加分を生成する
+    const mainMonthDays = DateService.generateMonthDays(mainMonth)
+  }
+
+  /**
+   * 1月分の日付を生成する
+   */
+  function _generateDays(yyyymm: YYYYMM) {
+    return DateService.generateMonthDays(yyyymm)
+  }
+
+  return {
+    currentMonth: mainMonth,
+    selectedDate,
+    calenderActions: {
+      prevMonth,
+      nextMonth,
+    },
+  }
+}
