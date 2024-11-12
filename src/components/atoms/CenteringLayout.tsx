6c0b2ffe1be9ea34091e1d8fefb0d2df0104be8f
sakho13 <wecl.khotaro@gmail.com>
2024-11-12 22:00:23 +0900

    ✨ CenteringLayoutコンポーネントにclassNameプロパティを追加し、スタイルのカスタマイズを可能に

--- src/components/atoms/CenteringLayout.tsx
+++ src/components/atoms/CenteringLayout.tsx
@@ -2,15 +2,17 @@ import { joinClassName } from "@/services/functions/joinClassName"
 
 type Props = {
   children: React.ReactNode
+  className?: string
 }
 
-export function CenteringLayout({ children }: Props) {
+export function CenteringLayout({ children, className }: Props) {
   return (
     <div
       className={joinClassName(
         "flex justify-center items-center",
         "h-full",
         "pt-4",
+        className ?? "",
       )}
     >
       {children}
