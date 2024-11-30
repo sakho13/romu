6adfe0c13dfddb53147ad8d4739b0abbe6d22c17
sakho13 <wecl.khotaro@gmail.com>
2024-11-30 17:54:47 +0900

    ✨ RomuHeaderコンポーネントにドロップダウンメニューを追加し、サインイン前と後のナビゲーションを改善

--- src/components/organisms/RomuHeader.tsx
+++ src/components/organisms/RomuHeader.tsx
@@ -19,6 +19,39 @@ export function RomuHeader({ type }: Props) {
   return (
     <div id='header' className='navbar border-b px-16'>
       <div className='navbar-start'>
+        <div className='dropdown'>
+          <div tabIndex={0} role='button' className='btn btn-ghost lg:hidden'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-5 w-5'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+            >
+              <path
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                stroke-width='2'
+                d='M4 6h16M4 12h8m-8 6h16'
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className='menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow'
+          >
+            <li>
+              <Link href={"/romu"}>Home</Link>
+            </li>
+            <li>
+              <Link href={"/romu/workout"}>Workout</Link>
+            </li>
+            <li>
+              <Link href={"/romu/analyze"}>Analyze</Link>
+            </li>
+          </ul>
+        </div>
+
         <a
           className='btn btn-ghost text-xl'
           href={type === "before-signed-in" ? "/" : "/romu"}
@@ -26,6 +59,22 @@ export function RomuHeader({ type }: Props) {
           RoMu
         </a>
       </div>
+
+      {type === "before-signed-in" ? null : (
+        <div className='navbar-center hidden lg:flex'>
+          <ul className='menu menu-horizontal'>
+            <li>
+              <Link href={"/romu"}>Home</Link>
+            </li>
+            <li>
+              <Link href={"/romu/workout"}>Workout</Link>
+            </li>
+            <li>
+              <Link href={"/romu/analyze"}>Analyze</Link>
+            </li>
+          </ul>
+        </div>
+      )}
 
       {type === "before-signed-in" ? (
         <Link className='navbar-end ' href={"/sign-in"}>
