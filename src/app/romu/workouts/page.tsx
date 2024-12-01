e11402c320a74ef8759a0cbee63cf1a9be26a705
sakho13 <wecl.khotaro@gmail.com>
2024-12-01 15:43:31 +0900

    ✨ RomuPageTitleコンポーネントを使用してワークアウトページのタイトルを更新

--- src/app/romu/workouts/page.tsx
+++ src/app/romu/workouts/page.tsx
@@ -1,14 +1,12 @@
 import { CenteringLayout } from "@/components/atoms/CenteringLayout"
+import { RomuPageTitle } from "@/components/atoms/RomuPageTitle"
 import { RomuWorkoutList } from "@/components/organisms/RomuWorkoutList"
-import { joinClassName } from "@/services/functions/joinClassName"
 
 export default function WorkoutsPage() {
   return (
     <CenteringLayout>
       <div className='flex flex-col items-center justify-center h-full mt-8'>
-        <h1 className={joinClassName("text-lg font-bold select-none")}>
-          登録済みワークアウト
-        </h1>
+        <RomuPageTitle title='登録済みワークアウト' />
 
         <RomuWorkoutList />
       </div>
