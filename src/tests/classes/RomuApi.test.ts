8ed7473c6ce4fdee025a89edf37943d611abda9d
sakho13 <wecl.khotaro@gmail.com>
2024-12-01 12:08:55 +0900

    ✅ RomuApiのテストを更新し、verifyAuthorizationHeaderメソッドのテストケースを追加

--- src/tests/classes/RomuApi.test.ts
+++ src/tests/classes/RomuApi.test.ts
@@ -10,7 +10,7 @@ describe("services/classes/RomuApi", () => {
   test("executeが成功する", async () => {
     const mainLogic = jest.fn()
     mainLogic.mockResolvedValue("success")
-    const api = new RomuApi("User-GET", {} as any)
+    const api = new RomuApi("User-GET")
 
     await expect(api.execute(mainLogic)).resolves.toEqual({
       status: 200,
@@ -26,7 +26,7 @@ describe("services/classes/RomuApi", () => {
     mainLogic.mockRejectedValueOnce(
       new RomuApiError({ errorCode: "AuthFailed", param: {} }),
     )
-    const api = new RomuApi("User-GET", {} as any)
+    const api = new RomuApi("User-GET")
 
     await expect(api.execute(mainLogic)).resolves.toEqual({
       status: 401,
@@ -45,7 +45,7 @@ describe("services/classes/RomuApi", () => {
   test("executeが失敗する Error", async () => {
     const mainLogic = jest.fn()
     mainLogic.mockRejectedValueOnce(new Error("error"))
-    const api = new RomuApi("User-GET", {} as any)
+    const api = new RomuApi("User-GET")
 
     await expect(api.execute(mainLogic)).resolves.toEqual({
       status: 400,
@@ -69,7 +69,7 @@ describe("services/classes/RomuApi", () => {
         param: { column: "ニックネーム", minLength: "1" },
       }),
     )
-    const api = new RomuApi("User-GET", {} as any)
+    const api = new RomuApi("User-GET")
 
     await expect(api.execute(mainLogic)).resolves.toEqual({
       status: 400,
@@ -84,5 +84,46 @@ describe("services/classes/RomuApi", () => {
         ],
       },
     })
+  })
+
+  test("verifyAuthorizationHeaderが成功する", async () => {
+    const api = new RomuApi("User-GET") as any
+    api.verifyFirebaseIdToken = jest.fn().mockResolvedValue("decoded")
+    const token = "Bearer token"
+    const result = await api.verifyAuthorizationHeader(token)
+    expect(result).toEqual("decoded")
+  })
+
+  test("verifyAuthorizationHeader デコードが失敗する", async () => {
+    const api = new RomuApi("User-GET") as any
+    api.verifyFirebaseIdToken = jest.fn().mockRejectedValue(new Error("error"))
+    const token = "Bearer token"
+    await expect(api.verifyAuthorizationHeader(token)).rejects.toThrow(
+      new RomuApiError({ errorCode: "AuthFailed", param: {} }),
+    )
+  })
+
+  test("verifyAuthorizationHeader tokenが空文字", async () => {
+    const api = new RomuApi("User-GET") as any
+    const token = ""
+    await expect(api.verifyAuthorizationHeader(token)).rejects.toThrow(
+      new RomuApiError({ errorCode: "AuthFailed", param: {} }),
+    )
+  })
+
+  test("verifyAuthorizationHeader tokenが数値", async () => {
+    const api = new RomuApi("User-GET") as any
+    const token = 123456
+    await expect(api.verifyAuthorizationHeader(token)).rejects.toThrow(
+      new RomuApiError({ errorCode: "AuthFailed", param: {} }),
+    )
+  })
+
+  test("verifyAuthorizationHeader tokenの形式「BearerToken」", async () => {
+    const api = new RomuApi("User-GET") as any
+    const token = "BearerToken"
+    await expect(api.verifyAuthorizationHeader(token)).rejects.toThrow(
+      new RomuApiError({ errorCode: "AuthFailed", param: {} }),
+    )
   })
 })
