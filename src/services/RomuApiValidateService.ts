eb55e28a41172246a56ddc31fc5473ec27d59904
sakho13 <wecl.khotaro@gmail.com>
2024-12-04 20:29:32 +0900

    ✨ RomuApiValidateServiceに複数のエラーチェック機能を追加し、必須パラメータの検証を強化

--- src/services/RomuApiValidateService.ts
+++ src/services/RomuApiValidateService.ts
@@ -1,6 +1,70 @@
 import { RomuApiError } from "./classes/RomuApiError"
+import { RomuApiErrors } from "./classes/RomuApiErrors"
+
+type RomuApiValidateReturn<T> =
+  | {
+      data: T
+      error: null
+    }
+  | {
+      data: null
+      error: RomuApiErrors
+    }
 
 export class RomuApiValidateService {
+  /**
+   * 複数のエラーチェックを全て実行する
+   * @description エラーチェックで発生した全てのエラーをまとめて返す
+   * @param checkFns エラーチェック関数の配列 (エラーが発生した場合は`RomuApiError`をthrowする)
+   * @returns `RomuApiErrors | null` エラーが発生しなかった場合はnullを返す
+   */
+  public static checkMultipleErrors(
+    checkFns: (() => void)[],
+  ): RomuApiErrors | null {
+    const errors = new RomuApiErrors()
+
+    checkFns.forEach((fn) => {
+      try {
+        fn()
+      } catch (error) {
+        if (error instanceof RomuApiError) {
+          errors.pushError(error.forRomuApiErrorsProp)
+        } else {
+          const err = new RomuApiError(
+            {
+              errorCode: "UnknownError",
+              param: {},
+            },
+            error,
+          )
+          errors.pushError(err.forRomuApiErrorsProp)
+        }
+      }
+    })
+
+    return errors.isEmpty ? null : errors
+  }
+
+  public static checkRequiredParameterInObject<K extends string>(
+    x: any,
+    keys: { column: K; name?: string }[],
+  ): RomuApiValidateReturn<{ [key in K]: any }> {
+    const error = this.checkMultipleErrors(
+      keys.map((key) => () => {
+        this.requiredParameter(x, key.column, key.name)
+      }),
+    )
+    return error
+      ? {
+          data: null,
+          error,
+        }
+      : {
+          data: x,
+          error: null,
+        }
+  }
+
   /**
    * 必須パラメータがオブジェクトにあるかどうかをチェックする
    * @param x オブジェクト
@@ -57,7 +121,7 @@ export class RomuApiValidateService {
     enumArray: E[],
     key: K,
     columnName?: string,
-  ) {
+  ): x is E {
     if (!enumArray.includes(x))
       throw new RomuApiError({
         errorCode: "InvalidInputEnum",
