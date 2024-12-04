3350fe19e3cf226ec582c9e928b1498e806316d1
sakho13 <wecl.khotaro@gmail.com>
2024-12-04 20:54:43 +0900

    ✨ CenteringLayoutコンポーネントにidプロパティを追加

--- src/components/atoms/CenteringLayout.tsx
+++ src/components/atoms/CenteringLayout.tsx
@@ -2,16 +2,19 @@ import { joinClassName } from "@/services/functions/joinClassName"
 
 type Props = {
   children: React.ReactNode
+  id?: string
   className?: string
 }
 
-export function CenteringLayout({ children, className }: Props) {
+export function CenteringLayout({ children, id, className }: Props) {
   return (
     <div
+      id={id}
       className={joinClassName(
         "flex justify-center items-center",
         "h-full",
         "pt-4",
+        "w-full",
         className ?? "",
       )}
     >
