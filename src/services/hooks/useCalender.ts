4049087521df8a82aa1ab73d291c1054b10d1377
sakho13 <wecl.khotaro@gmail.com>
2025-01-01 18:39:18 +0900

    ⚰️ useCalenderから未使用のコードを削除し、可読性を向上

--- src/services/hooks/useCalender.ts
+++ src/services/hooks/useCalender.ts
@@ -8,32 +8,6 @@ export function useCalender(initDate: Date = new Date()) {
     setCurrentMonth(date)
   }
 
-  // const [selectedDate] = useState<YYYYMMDD>(
-  //   DateService.dateToYYYYMMDD(initDate),
-  // )
-  // const [weeks, setWeeks] = useState<number[][]>([])
-
-  // function prevMonth() {
-  //   setMainMonth(DateService.shiftMonth(mainMonth, -1))
-  // }
-
-  // function nextMonth() {
-  //   setMainMonth(DateService.shiftMonth(mainMonth, +1))
-  // }
-
-  // function initCalender(mainMonth: YYYYMM) {
-  //   // 前後の月分も生成する
-  //   // 月を移動してした場合、追加分を生成する
-  //   const mainMonthDays = DateService.generateMonthDays(mainMonth)
-  // }
-
-  /**
-   * 1月分の日付を生成する
-   */
-  // function _generateDays(yyyymm: YYYYMM) {
-  //   return DateService.generateMonthDays(yyyymm)
-  // }
-
   return {
     currentMonth,
     onChangeMonth,
